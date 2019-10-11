e6b53c19a473a596e5f3002f5e1933b6e6777801
InsideU <umang20anku@gmail.com>
2019-10-11 21:54:32 +0530

    question_fetched_success

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const mysql =require('mysql');
+const mysql = require('mysql');
 
-const app=express();
+const app = express();
 
 //downloading the zip file from the system 
-app.get('/downloads',(req,res)=>{
+app.get('/downloads', (req, res) => {
     console.log("Into the download section");
-    var filepath=__dirname+'/downloads/RC2.exe';
+    var filepath = __dirname + '/downloads/RC2.exe';
     res.download(filepath)
 });
 //end of the zip file downloading
@@ -15,26 +15,26 @@ app.get('/downloads',(req,res)=>{
 // create connection 
 
 const db = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'rollcall'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'rollcall'
 });
 //connect 
-db.connect((err)=>{
-    if(err){
+db.connect((err) => {
+    if (err) {
         throw err;
     }
     console.log('mysql connected');
-    
+
 });
 
 // routing condition 
 
-app.get('/getposts',(req,res)=>{
-    let sql="Select * from token";
-    let query =db.query(sql,(err,results)=>{
-        if(err) throw err;
+app.get('/getposts', (req, res) => {
+    let sql = "Select * from token";
+    let query = db.query(sql, (err, results) => {
+        if (err) throw err;
         console.log(results);
         res.send(results);
     });
@@ -44,7 +44,7 @@ app.get('/getposts',(req,res)=>{
 
 
 
-app.listen('3000',()=>{
+app.listen('3000', () => {
     console.log('Server started on port 3000');
 });
 
--- questionsfetch.js
+++ questionsfetch.js
@@ -1,23 +1,31 @@
 const express = require('express');
-const mysql =require('mysql');
+const mysql = require('mysql');
 
-const app=express();
-app.use('/download',express.static('./files/download'))
+const app = express();
+app.use('/download', express.static('./files/download'))
 //downloading the zip file from the system 
-app.get('/downloads',(req,res)=>{
-    db.connect((err)=>{
-        var parameters=req.query;
-       const teamnumber=parameters.teamno;
-       //console.log(teamnumber);
-        if(err){
+app.get('/downloads', (req, res) => {
+    db.connect((err) => {
+        var parameters = req.query;
+        const teamnumber = parameters.teamno;
+        //console.log(teamnumber);
+        if (err) {
             console.log("Wait for the questions to be alloted");
         }
-        let sql1=`select * from allotment where teamid = ${teamnumber}` ;
-        db.query(sql1,(err,results)=>{
-            if(err) throw err;
-            results.forEach((elements)=>{
-                var questionnumbers=elements.questionno;
-                console.log(questionnumbers);
+        let sql1 = `select * from allotment where teamid = ${teamnumber}`;
+        db.query(sql1, (err, results) => {
+            if (err) throw err;
+            results.forEach((elements) => {
+                var questionnumbers = elements.questionno;
+                console.log(questionnumbers)
+                //see this only one question location is printed
+                let sql2 = `select (route) from routetest where qno=${questionnumbers}`;
+                console.log(sql2)
+
+                db.query(sql2, (err, results) => {
+                    if (err) throw err;
+                    console.log(results);
+                });
             });
             //  var filename=results[0].questionno
             //  console.log(results[0].questionno)
@@ -27,30 +35,30 @@ app.get('/downloads',(req,res)=>{
             //     mac:`/download/${filename}/run.o`,
             // });
         });
-        
+
         // console.log('You Have been alloted a Question');
         // var filepath=__dirname+'/downloads/RC2.exe';
         // res.download(filepath)
     }); //end of the database connection
-   
+
 });//end of the zip file downloading
 
 
 // create connection 
 
 const db = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'revcoding'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'revcoding'
 });
 //connect 
-db.connect((err)=>{
-    if(err){
+db.connect((err) => {
+    if (err) {
         throw err;
     }
-   // console.log('mysql connected');
-    
+    // console.log('mysql connected');
+
 });
 
 // routing condition 
@@ -61,7 +69,7 @@ db.connect((err)=>{
 
 
 
-app.listen('3000',()=>{
+app.listen('3000', () => {
     console.log('Server started on port 3000');
 });
 
