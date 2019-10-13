2390d8a41102fd5c1b3054ffdee8caf6abd72356
InsideU <umang20anku@gmail.com>
2019-10-13 17:37:27 +0530

    submission_linked_to_database_bidding_applied

--- submission.js
+++ submission.js
@@ -3,6 +3,7 @@ const app=express();
 const mysql=require('mysql');
 const multer = require('multer');
 judge = require('./judge')
+var teamNumber;
 
 // storing the current file location to be executed in the database
 // app.post('/api/userfile',function(req,res){
@@ -23,7 +24,7 @@ var storage= multer.diskStorage({
         callback(null,'./uploads');
     },
     filename: function (req, file, callback) {
-        const teamNumber = req.query.teamno;
+        teamNumber = req.query.teamno;
         const questionNumber=req.query.qno;
         global.filetypes= req.query.type;
         filename=`${questionNumber}_${teamNumber}` + '-' + Date.now()+`.${filetypes}`;
@@ -48,13 +49,13 @@ var storage= multer.diskStorage({
         // const parameters = req.query;
         // const teamnumbers = parameters.teamno;
         // const questionnumbers= parameters.qno;
-        // console.log(teamnumbers);
         // console.log(questionnumbers)
         uploads(req,res,function(err) {
             if(err) {
                 console.log(err.message)
                 return res.end("Error uploading file.");
             }
+           
             // console.log("this is the ",`${filetypes}`)
         //    console.log(req.file.filename)  // current uploaded filename
            judge( `./uploads/${filename}`, [
@@ -64,6 +65,7 @@ var storage= multer.diskStorage({
             `D:\\nodejs\\reverse_coding\\reverse_codingv2\\files\\ques\\1\\test\\O1.txt`,
             `D:\\nodejs\\reverse_coding\\reverse_codingv2\\files\\ques\\1\\test\\O2.txt`,
         ], `${filetypes}`).then((result) => {
+            
             // console.log({
             //     err: false,
             //     result, points: 150
@@ -74,12 +76,49 @@ var storage= multer.diskStorage({
                 if(elements==true){
                     count+=1
                 }
-
-                console.log({
-                    counts : count ,
-                    points: (count/2)*150
+                });
+                var points = (count/4)*150;
+                // console.log({
+                //     counts : count ,
+                //     points: (count/2)*150
+                // })
+                db.connect((err)=>{
+                    if(err) throw err;
+                console.log(teamNumber);
+                let sql3=`select * from score where Teamid=${teamNumber}`;
+                db.query(sql3,(err,result)=>{
+                    if(result.length==0){
+                        let sql = `insert into score values(${teamNumber},${points})`;
+                        db.query(sql,(err,results)=>{
+                            if(err) console.log(err.message);
+                        console.log("inserted the field successfully",results); 
+                     });
+                    }
+                    else{
+                        let marks=`select (score) from score where Teamid=${teamNumber}`;
+                        db.query(marks,(err,results)=>{
+                            if(err) throw err;
+                        var last_score=results[0].score;
+                        uscore=update_score(last_score,`${points}`);
+                            
+                        let sql2=`update score set score=${uscore} where Teamid=${teamNumber}`;
+                        db.query(sql2,(err,result1)=>{
+                            if(err) throw err;
+                        console.log(result1);
+                        })
+                        });
+                        
+                  
+                    }
+                
+                
+                       
+                // console.log(result)
+                    // }
                 })
-            })
+
+                
+            });
         }).catch(e => {
             console.log({
                 err: true,
@@ -109,10 +148,19 @@ const db = mysql.createConnection({
 
 
 //connect 
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    // console.log('mysql connected');
+// db.connect((err) => {
+//     if (err) {
+//         throw err;
+//     }
+//     // console.log('mysql connected');
 
-});
+// });
+function update_score(last_score,present_score){
+    
+    if(last_score > present_score){
+        return last_score;
+    }
+    else{
+        return (present_score);
+    }
+}
